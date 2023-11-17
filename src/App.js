1724187285a2cb6fd2a0e699c62b4b7689f44fc9
Skyboy14 <96543258+Skyboy14@users.noreply.github.com>
2023-11-17 13:07:14 +0530

    updated Css

--- src/App.js
+++ src/App.js
@@ -4,14 +4,25 @@ import { Provider } from 'react-redux';
 import { store, persistor } from '../src/redux/store';
 import TodoList from '../src/components/TodoList';
 import { PersistGate } from 'redux-persist/integration/react';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+  border: 2px solid red;
+  width: fit-content;
+  margin: auto;
+  padding: 25px 25px 50px;
+  border-radius: 25px;
+  margin-top: 20px;
+  background-color: azure;
+`
 
 function App() {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <div className="App">
+        <StyledDiv>
           <TodoList />
-        </div>
+        </StyledDiv>
       </PersistGate>
     </Provider>
   );
